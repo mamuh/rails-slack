e2ed0b773d7640c08a2e88f96dfcad0f297a7acb
Mariana Caramori <musardo@mail.com>
2020-08-17 14:41:54 -0300

    set api route with name param

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,9 +1,15 @@
 class Api::V1::MessagesController < ActionController::Base
+  before_action :find_channel
+
   def index
-    @messages = Message.all
+    @messages = @channel.messages
     render json: @messages
   end
 
   def create
+  end
+
+  def find_channel
+    @channel = Channel.find_by_name(params[:name])
   end
 end
