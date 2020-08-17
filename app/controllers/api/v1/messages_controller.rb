0298a018cefa6d5aeb5e436873b8082cde43bdbd
Mariana Caramori <musardo@mail.com>
2020-08-17 14:55:35 -0300

    before action find channel

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::MessagesController < ActionController::Base
   before_action :find_channel
 
   def index
-    @channel = Channel.find_by_name(params[:channel_name])
     @messages = @channel.messages
     render json: @messages
   end
@@ -11,6 +10,6 @@ class Api::V1::MessagesController < ActionController::Base
   end
 
   def find_channel
-    @channel = Channel.find_by_name(params[:name])
+    @channel = Channel.find_by_name(params[:channel_name])
   end
 end
