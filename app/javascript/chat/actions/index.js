03876e34671965ce84d5fb3566fa388865424a40
Mariana Caramori <musardo@mail.com>
2020-10-04 13:28:21 +0100

    removed base url

--- app/javascript/chat/actions/index.js
+++ app/javascript/chat/actions/index.js
@@ -1,13 +1,13 @@
 // import { createBrowserHistory as history } from 'history';
 
-const BASE_URL = window.location.origin.toString();
+// const BASE_URL = window.location.origin.toString();
 
 export const FETCH_MESSAGES = 'FETCH_MESSAGES';
 export const MESSAGE_POSTED = 'MESSAGE_POSTED';
 export const CHANNEL_SELECTED = 'CHANNEL_SELECTED';
 
 export function fetchMessages(channel) {
-  const url = `${BASE_URL}/api/v1/channels/${channel}/messages`;
+  const url = `/api/v1/channels/${channel}/messages`;
   const promise = fetch(url, { credentials: "same-origin" }).then(r => r.json());
 
   return {
@@ -18,7 +18,7 @@ export function fetchMessages(channel) {
 
 
 export function createMessage(channel, content) {
-  const url = `${BASE_URL}/api/v1/channels/${channel}/messages`;
+  const url = `/api/v1/channels/${channel}/messages`;
   const body = { content };
   const csrfToken = document.querySelector('meta[name="csrf-token"]').attributes.content.value;
   const promise = fetch(url, {
